ef4c1731e003f2ff22fc833c4436ff323370c414
Hamza Bouthour <hbouthour@SIS.local>
2023-08-05 12:52:31 -0700

    feature: implement mysql db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "sql.freedb.tech",
+  port: 3306,
+  database: "freedb_hbxTesting",
+  user: "freedb_hbxTester",
+  password: "&FD!7UQTJvj4yps",
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  try {
+    db.query("SELECT * FROM hbx_programs LIMIT 1", (err, result) => {
+      err ? console.log(err) : res.send(result);
+    });
+  } catch (err) {
+    console.log("DB connexion failed!");
+  }
 });
 
 module.exports = router;
